aa2fd9f68ef12179acb4fa883d035ff97b87bea1
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-01 20:58:27 +0400

    lr3

--- lr3/task5.sql
+++ lr3/task5.sql
@@ -2,4 +2,4 @@ USE cd;
 SELECT DISTINCT firstname, surname FROM members
 JOIN bookings ON members.memid = bookings.memid
 JOIN facilities ON facilities.facid = bookings.facid
-WHERE facilities.facility LIKE '%Tennis court%';
+WHERE facilities.facility LIKE '%Tennis court 1%' OR facilities.facility LIKE '%Tennis court 2%';
