f18c304a64e488dbe10b80084693e53756786d2f
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-01 20:52:30 +0400

    lr2

--- lr2/task11.sql
+++ lr2/task11.sql
@@ -1,5 +1,5 @@
 /*создаём новый столбец, называем его "расположение" и в нём указываем то, откуда был взят объект, после объединяем две таблицы через UNION*/
 USE cd;
-SELECT firstname AS 'объединение'  FROM members
-UNION
-SELECT facility AS 'объект' FROM facilities;
+SELECT firstname AS 'Firstname/Facilities'  FROM members
+UNION ALL
+SELECT facility  FROM facilities;
