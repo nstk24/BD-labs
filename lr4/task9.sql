928f7b055a5a8fefed5cea5c182447ebed6d07a4
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-03 11:56:47 +0400

    lr3,4

--- lr4/task9.sql
+++ lr4/task9.sql
@@ -1,3 +1,6 @@
 USE cd;
 SELECT * from members
 where memid not in(select distinct memid from bookings);
+DELETE FROM members
+WHERE memid not in (select memid from bookings);
+select * from members where memid = 37;
