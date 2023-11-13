a116b243d9c636b4e200a164a042f618b39744a3
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-13 13:00:57 +0400

    LR3

--- lr3/task7.sql
+++ lr3/task7.sql
@@ -1,7 +1,9 @@
-/*Выберите список всех членов, включая человека, который их рекомендовал*/
- USE cd;
-SELECT CONCAT(mem.firstname,' ',mem.surname) AS membername,
-(SELECT CONCAT(rec.firstname,' ',rec.surname) FROM members rec WHERE mem.recommendedby=rec.memid) AS recname 
+USE cd; /* Выбор активной базы данных 'cd'*/
+/*Выборка имени и фамилии членов клуба, а также имен и фамилий тех, кто их рекомендовал*/
+SELECT CONCAT(mem.firstname, ' ', mem.surname) AS membername, /*Выборка имени и фамилии членов клуба, а также имен и фамилий тех, кто их рекомендовал*/
+(SELECT CONCAT(rec.firstname, ' ', rec.surname) FROM members rec WHERE mem.recommendedby = rec.memid) AS recname
 FROM members mem
-WHERE mem.memid!=0
-ORDER BY membername;
+/* Отбор только тех членов, у которых memid не равен 0*/
+WHERE mem.memid != 0
+/* Сортировка результатов по имени и фамилии члена клуба*/
+ORDER BY mem.firstname;
