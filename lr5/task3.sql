68ce50572eca4a22d19dc421ca27f4daca59ec5a
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-13 10:56:40 +0400

    lr5

--- lr5/task3.sql
+++ lr5/task3.sql
@@ -1,2 +1,9 @@
 USE cd;
-SELECT recommendedby, COUNT(*) FROM members WHERE recommendedby IS NOT NULL GROUP BY recommendedby;
+SELECT mem.memid AS MemberID,
+mem.firstname AS firstname,
+mem.surname AS lastname,
+COUNT (mem2.memid) AS rec_count
+FROM members mem
+LEFT JOIN members mem2 ON mem.memid = mem2.recommendedby
+WHERE mem.recommendedby IS NOT NULL
+GROUP BY mem.memid, mem.firstname, mem.surname;
