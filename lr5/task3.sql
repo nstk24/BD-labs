6f96841de431f1f788af0be011217b1495b68007
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-13 11:11:44 +0400

    lr5

--- lr5/task3.sql
+++ lr5/task3.sql
@@ -4,6 +4,5 @@ mem.firstname AS firstname,
 mem.surname AS lastname,
 COUNT (mem2.memid) AS rec_count
 FROM members mem
-LEFT JOIN members mem2 ON mem.memid = mem2.recommendedby
-WHERE mem.recommendedby IS NOT NULL
+JOIN members mem2 ON mem.memid = mem2.recommendedby
 GROUP BY mem.memid, mem.firstname, mem.surname;
