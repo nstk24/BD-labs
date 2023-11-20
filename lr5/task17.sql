38b3ec6d0768a570001332cd6e91282cd102ab75
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-20 20:42:13 +0400

    12

--- lr5/task17.sql
+++ lr5/task17.sql
@@ -3,7 +3,7 @@ USE cd;
 
 -- Выбираем уникальные комбинации количества бронирований, группы доходов и названия объекта
 -- В зависимости от суммы membercost и guestcost объекта выбираем группу доходов ('Высокие доходы', 'Средние доходы' или 'Низкие доходы')
--- Группируем результаты по идентификатору участника (m.memid), группе доходов (income_group) и названию объекта (facility_name)
+-- Группируем результаты по  группе доходов (income_group) и названию объекта (facility_name)
 -- Сортируем результаты по группе доходов и названию объекта
 SELECT DISTINCT
     CASE
@@ -19,6 +19,6 @@ LEFT JOIN
 LEFT JOIN
     facilities f ON b.facid = f.facid
 GROUP BY
-    m.memid, income_group, facility_name
+     income_group, facility_name
 ORDER BY
     income_group, facility_name;
