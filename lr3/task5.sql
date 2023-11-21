e8d095ffbdee068f9e80e789470111aaf8df7034
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-21 10:44:57 +0400

    13

--- lr3/task5.sql
+++ lr3/task5.sql
@@ -3,4 +3,4 @@ USE cd;
 SELECT DISTINCT CONCAT(firstname," ",surname) AS "membername"  FROM members
 JOIN bookings ON members.memid = bookings.memid
 JOIN facilities ON facilities.facid = bookings.facid
-WHERE facilities.facility LIKE "%Court 1%" OR facilities.facility LIKE "Court 2";
+WHERE facilities.facility LIKE "%Court 1%" OR facilities.facility LIKE "%Court 2%";
--- lr3/task6.sql
+++ lr3/task6.sql
@@ -1,6 +1,6 @@
 /*Выберите список бронирований на 14 сентября 2012 г.*/
 USE cd;
-SELECT DISTINCT CONCAT(f.facility, ' ', m.firstname) AS facilityName, 
+SELECT DISTINCT CONCAT(f.facility, ' ', m.firstname) AS facilityName, starttime,
 CASE 
 WHEN b.memid = 0 THEN f.guestcost * b.slots
 ELSE f.membercost * b.slots 
@@ -8,7 +8,7 @@ END AS cost
 FROM bookings b
 JOIN facilities f ON b.facid = f.facid
 JOIN members m ON b.memid = m.memid
-WHERE DATE(b.starttime) = '2012-09-14' AND
+WHERE b.starttime > '2012-09-13 23:59:59' AND b.starttime <='2012-09-14 23:59:59' AND
 ((b.memid = 0 AND  f.guestcost * b.slots > 30) OR
 (b.memid != 0 AND f.membercost * b.slots > 30)) 
-ORDER BY cost DESC;
+ORDER BY cost DESC ;
