59ec05ab620557f442925fd809b28e41c686240b
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-21 11:15:01 +0400

    32

--- lr4/task9.sql
+++ lr4/task9.sql
@@ -1,4 +1,5 @@
 USE cd;
+--выбираем из members тех членов, которые не бронировали ничего, а затем удаляем их, потом проверяем есть ли в members член с id = 37
 SELECT * from members
 where memid not in(select distinct memid from bookings);
 DELETE FROM members
