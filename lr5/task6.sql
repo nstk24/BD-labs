334ad6e4d044d7662ad40fa9af45cf4b3616436d
Sergey Nastkin <nastkinsergej24@gmail.com>
2023-11-25 22:23:18 +0400

    1

--- lr5/task6.sql
+++ lr5/task6.sql
@@ -1,9 +1,8 @@
 -- Используем базу данных "cd"
 USE cd;
-
 -- Выбираем месяц бронирования, идентификатор объекта (facid) и общее количество бронирований для каждого месяца и объекта
 -- за период 2012 года
-<<<<<<< HEAD
+
 USE cd;
 SELECT f.facility, MONTH(b.starttime) AS 'Месяц', SUM(b.slots) AS 'Количество бронирований' FROM facilities f
 INNER JOIN bookings b ON f.facid = b.facid WHERE YEAR(b.starttime) = 2012
@@ -13,4 +12,4 @@ SELECT EXTRACT(MONTH FROM starttime) AS MONTH, bookings.facid, COUNT(*) AS TOTAL
 FROM bookings
 WHERE YEAR(starttime) = 2012
 GROUP BY MONTH, facid;
->>>>>>> 60c7b0f216c1386be9810715953adca6013f4763
+
